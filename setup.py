5421f86c0db2019d4786d6f5a44e0455a90422fb
Chuneeta <cnunhokee@gmail.com>
2025-07-02 11:13:34 +0800

    fixed setup parameters

--- setup.py
+++ setup.py
@@ -1,26 +1,47 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from mwa_cal import version
+import os
+import json
 
-setup(
-    name="autocal",  # Package name
-    version="0.1.0",  # Version number
-    author="Ridhima Nunhokee",
-    author_email="cnunhokee@gmail.com",
-    description="Bandpass calibration using autocorrelations",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/Chuneeta/autocal",  # URL of your project
-    packages=find_packages(),  # Automatically find all packages
-    install_requires=[
-        "numpy",  # List dependencies here
-        "pandas",
-		"matplotlib",
-		"astropy"
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-)
+data = [version.git_origin, version.git_hash,
+        version.git_description, version.git_branch]
+with open(os.path.join('mwa_cal', 'GIT_INFO'), 'w') as outfile:
+    json.dump(data, outfile)
+
+
+def package_files(package_dir, subdirectory):
+    # walk the input package_dir/subdirectory
+    # return a package_data list
+    paths = []
+    directory = os.path.join(package_dir, subdirectory)
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            path = path.replace(package_dir + '/', '')
+            paths.append(os.path.join(path, filename))
+    return paths
+
+
+data_files = package_files('mwa_cal', 'data')
+
+setup_args = {
+    'name':         'mwa_cal',
+    'author':       'Chuneeta Nunhokee',
+    'url':          'https://github.com/Chuneeta/mwa_cal',
+    'license':      'BSD',
+    'version':      version.version,
+    'description':  'MWA Autocorrelation Calibration.',
+    'packages':     ['mwa_cal'],
+    'package_dir':  {'mwa_cal': 'mwa_cal'},
+    'package_data': {'mwa_cal': data_files},
+    'install_requires': ['numpy', 'scipy',
+                         'astropy>3.0.0', 'matplotlib>=2.2',
+                         'python_dateutil>=2.6.0',
+                         'pytest','mwa_qa'],
+    'include_package_data': True,
+    'zip_safe':     False,
+    'scripts': ['scripts/run_autocal.py']
+}
+
+if __name__ == '__main__':
+    setup(*(), **setup_args)
 
